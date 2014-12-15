b46c04d62808075cf30e1f05bc55b8ce30931da1
Leonardo Barros <nickolback@gmail.com>
2014-12-14 22:56:21 -0200

    Exemplo usando valores vazios
    Foi usaldo o método nil?

---
+++ valor_nil.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+#Exemplo baseado no livro de Ruby (eBook)
+#aprenda a programar na linguagem mais divertida
+#posicao 504
+
+seu_nome = nil
+seu_endereco = nil
+seu_telefone = nil
+
+print "Nome: "
+seu_nome = gets.chomp
+
+print "Endereco: "
+seu_endereco = gets.chomp
+
+print "Telefone: "
+seu_telefone = gets.chomp
+
+puts "Seu nome: #{seu_nome} e o seu endereco: #{seu_endereco}
+      seu telefone: #{seu_telefone}"
