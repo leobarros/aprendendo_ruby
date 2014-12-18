7fd4737b8d3dec04a7574fc0dad88271447b8a2e
Leonardo Barros <nickolback@gmail.com>
2014-12-18 21:59:40 -0200

    Copiando os valores do metodo initialize
    Criando variáveis de instância.

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -4,5 +4,18 @@
 class Livro
   def initialize(autor, isbn = "1", numero_de_paginas)
     puts "Autor: #{autor}, ISBN: #{isbn}, Pag: #{numero_de_paginas}"
+
+      #copiando os valores dos parametros do metodo initialize
+      #para variaveis de instancia
+      
+      @autor = autor
+      @isbn = isbn
+      @numero_de_paginas = numero_de_paginas
   end
+
+ teste_e_design = Livro.new("Mauricio Aniche", "123454", 247)
+ web_design_responsivo = Livro.new("Tarcio Zemel", "452565", 321)
+
+ p teste_e_design
+ p web_design_responsivo
 end
