66849fe89b054a813fbfe7495402af14893d91eb
Leonardo Barros <nickolback@gmail.com>
2014-12-20 19:32:53 -0200

    Tornando o acesso ao livro publico
    Criando um método publico com attr_reader

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -2,11 +2,14 @@
 #encoding: utf-8
 
 class Biblioteca
-
+  
+  #tornando público
+  attr_reader :livros
+  
   def initialize
     @livros = []
   end
-
+  
   def adiciona(livros)
     @livros << livros
   end
