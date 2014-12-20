55931a6564670bdbc7a75af9f9e505b4bf17679b
Leonardo Barros <nickolback@gmail.com>
2014-12-20 19:03:36 -0200

    Limpando o código e usando o array
    Fiz uma limpeza no código e guardando tudo em array

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -2,33 +2,31 @@
 # encoding: utf-8
 
 class Livro
-	
-	attr_accessor :preco
+  attr_accessor :preco
 
   def initialize(autor, isbn = "1", numero_de_paginas, preco)
-    puts "Autor: #{autor}, ISBN: #{isbn}, Pag: #{numero_de_paginas}"
-
-      #copiando os valores dos parametros do metodo initialize
-      #para variaveis de instancia
+    #copiando os valores dos parametros do metodo initialize
+    #para variaveis de instancia
       
       @autor = autor
       @isbn = isbn
       @numero_de_paginas = numero_de_paginas
       @preco = preco
   end
-
+  
   def to_s
-  	"Autor: #{@autor}, ISBN: #{@isbn}, Paginas: #{@numero_de_paginas}"
+    "Autor: #{@autor}, ISBN: #{@isbn}, Paginas: #{@numero_de_paginas}"
   end
 end
+ 
+ # guardar os dados em um array
 
- teste_e_design = Livro.new("Mauricio Aniche", "123454", 247, 60.9)
- web_design_responsivo = Livro.new("Tarcio Zemel", "452565", 321, 69.0)
+  biblioteca = []
+ 
+  teste_e_design = Livro.new("Mauricio Aniche", "123454", 247, 60.9)
+  web_design_responsivo = Livro.new("Tarcio Zemel", "452565", 321, 69.0)
 
- puts teste_e_design
- #Pegando o valor do livro teste_e_design
- puts "Preço: #{teste_e_design.preco}"
+  biblioteca << teste_e_design
+  biblioteca << web_design_responsivo
 
- puts web_design_responsivo
- #Pegando o valor do livro web_design_resposivo
- puts "Preço: #{web_design_responsivo.preco}"
+  puts biblioteca
