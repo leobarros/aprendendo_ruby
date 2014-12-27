16fa6a9ee2a8db72ba3bf81429fbb4ab47ed232d
Leonardo Barros <nickolback@gmail.com>
2014-12-27 17:48:34 -0200

    Retirada dos espaços em branco
    Foi feita uma limpeza dos espaços em branco :)

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -2,18 +2,16 @@
 #encoding: utf-8
 
 class Biblioteca
-  
+
   #tornando público
   attr_reader :livros
-  
   def initialize
     @livros = {} #Inicializa com um hash 
   end
-  
+
   def adiciona(livros)
     @livros[livro.categoria] ||=[]
     @livros[livro.categoria] << livros
   end
 
 end
-
