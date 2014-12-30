02ff8319dd1ea08d8554b8bb4f52d63375318d1b
Leonardo Barros <nickolback@gmail.com>
2014-12-29 22:21:03 -0200

    Sobrescrevendo o método eql?
    Fazendo comparações dos livros

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -2,31 +2,37 @@
 # encoding: utf-8
 
 class Livro
-   
+
   attr_accessor :valor
   attr_reader :categoria
-   
+  attr_reader :isbn
+
   def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
     #copiando os valores dos parametros do metodo initialize
     #para variaveis de instancia
-      
+
       @autor = autor
       @isbn = isbn
       @numero_de_paginas = numero_de_paginas
       @valor = valor
       @categoria = categoria
-      
+
   end
-  
+
   def to_s
     "Autor: #{@autor}, ISBN: #{@isbn}, Paginas: #{@numero_de_paginas}, Categoria: #{@categoria}"
   end
+
+  def eql? (outro_livro)
+    @isbn == outro_livro.isbn
+  end
+
 end
- 
+
  # guardar os dados em um array
 
   biblioteca = []
- 
+
   teste_e_design = Livro.new("Mauricio Aniche", "123454", 247, 60.9)
   web_design_responsivo = Livro.new("Tarcio Zemel", "452565", 321, 69.0)
 
