d767b05cdbe59aa3eae8d09137de1b7bc4c5e04f
Leonardo Barros <nickolback@gmail.com>
2015-02-08 08:29:49 -0200

    Criando o método titulo na classe

--- loja_virtual/lib/relatorio.rb
+++ loja_virtual/lib/relatorio.rb
@@ -9,3 +9,14 @@ class Relatorio
   def total
     @biblioteca.livros.inject(0) { |tot, livro| tot += livro.valor }
   end
+
+  def titulos
+    titulos = []
+
+    @biblioteca.livros.each do |livro|
+      titulos << livro.titulo
+    end
+    titulos
+  end
+end
+
