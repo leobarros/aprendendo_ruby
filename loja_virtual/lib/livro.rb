a8feb5664874392d335a865949c2f5fc6fc21a92
Leonardo Barros <nickolback@gmail.com>
2015-02-08 08:23:07 -0200

    Adicionando o atributo titulo no método

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -6,15 +6,16 @@ class Livro
   attr_accessor :valor
   attr_reader :categoria, :autor, :titulo
 
-  def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
+  def initialize(titulo, autor, isbn = "1", numero_de_paginas, valor, categoria)
     #copiando os valores dos parametros do metodo initialize
     #para variaveis de instancia
 
-      @autor = autor
-      @isbn = isbn
-      @numero_de_paginas = numero_de_paginas
-      @valor = valor
-      @categoria = categoria
+    @titulo = titulo
+    @autor = autor
+    @isbn = isbn
+    @numero_de_paginas = numero_de_paginas
+    @valor = valor
+    @categoria = categoria
 
   end
 
