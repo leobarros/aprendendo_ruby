d5666ad92d7b37d1aedbe001d359a2e560551184
Leonardo Barros <nickolback@gmail.com>
2015-02-08 08:08:03 -0200

    Melhorando o código com inject

--- loja_virtual/lib/relatorio.rb
+++ loja_virtual/lib/relatorio.rb
@@ -7,11 +7,5 @@ class Relatorio
   end
 
   def total
-    soma = 0.0
-
-    @biblioteca.livros.each do |livro|
-      soma += livro.valor
-    end
-    soma
+    @biblioteca.livros.inject(0) { |tot, livro| to += livro.valor }
   end
-end
