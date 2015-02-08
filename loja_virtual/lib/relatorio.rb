80f6c39ca88569f3000b155e9b00b52a9206fa5c
Leonardo Barros <nickolback@gmail.com>
2015-02-08 07:47:23 -0200

    Criando a classe Relatório

---
+++ loja_virtual/lib/relatorio.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+class Relatorio
+  def initialize(biblioteca)
+    @biblioteca = biblioteca
+  end
+
+  def total
+    soma = 0.0
+
+    @biblioteca.livros.each do |livro|
+      soma += livro.valor
+    end
+    soma
+  end
+end
